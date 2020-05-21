5ef8fe41380d8a9d0405644934f91db81808667c
sammydentino <sammydentino@outlook.com>
2020-05-20 20:43:57 -0500

    Initial Commit

---
+++ Clear/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Clear
+//
+//  Created by Sammy Dentino on 5/20/20.
+//  Copyright © 2020 Sammy Dentino. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
